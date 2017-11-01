52a842202e890d9f035fdde51d7c268fc8c682b2
zukethenuke <zukethenuke@hotmail.com>
2017-10-31 21:14:58 -0500

    Add app_startup_data endpoint

--- app/controllers/api/nd_wells_controller.rb
+++ app/controllers/api/nd_wells_controller.rb
@@ -1,12 +1,12 @@
 class Api::NdWellsController < ApplicationController
   def index
-    if params
+    # if params
 
-    else
+    # else
       @wells = NdWell.all
       # render 'index.json.jbuilder'
       render json: {wells: @wells}
-    end
+    # end
   end
 
   def show
@@ -24,7 +24,17 @@ class Api::NdWellsController < ApplicationController
   end
 
   def all_operators
-    @operators = NdWell.select(:current_operator).distinct
+    # @operators = NdWell.select(:current_operator).distinct # return array of objects with id & current_operator
+    @operators = NdWell.pluck(:current_operator).uniq # returns array of current_operators
     render json: {operators: @operators}
+  end
+
+  def all_lease_names
+    @leases = NdWell.pluck(:lease_name)
+  end
+
+  def app_startup_data
+    @data = NdWell.select(:current_operator, :current_well_name, :lease_name, :lease_number).where.not(well_status: 'DRY')
+    render json: {wells: @data}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get '/wells' => 'nd_wells#index'
     get '/wells/random' => 'nd_wells#random'
     get '/wells/all_operators' => 'nd_wells#all_operators'
+    get '/wells/startup' => 'nd_wells#app_startup_data'
     get '/wells/:id' => 'nd_wells#show'
   end
 end
