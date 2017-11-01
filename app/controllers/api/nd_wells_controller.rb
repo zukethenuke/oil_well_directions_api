8be89c3f1ba785f413f54bc72c24d6834b2b397a
zukethenuke <zukethenuke@hotmail.com>
2017-10-31 19:38:40 -0500

    Add all_operators api route

--- app/controllers/api/nd_wells_controller.rb
+++ app/controllers/api/nd_wells_controller.rb
@@ -1,8 +1,12 @@
 class Api::NdWellsController < ApplicationController
   def index
-    @wells = NdWell.all
-    # render 'index.json.jbuilder'
-    render json: {wells: @wells}
+    if params
+
+    else
+      @wells = NdWell.all
+      # render 'index.json.jbuilder'
+      render json: {wells: @wells}
+    end
   end
 
   def show
@@ -17,5 +21,10 @@ class Api::NdWellsController < ApplicationController
   def random
     @wells = NdWell.limit(20).order("RANDOM()")
     render json: {wells: @wells}
+  end
+
+  def all_operators
+    @operators = NdWell.select(:current_operator).distinct
+    render json: {operators: @operators}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/wells' => 'nd_wells#index'
     get '/wells/random' => 'nd_wells#random'
+    get '/wells/all_operators' => 'nd_wells#all_operators'
     get '/wells/:id' => 'nd_wells#show'
   end
 end
